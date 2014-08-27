a26a7552442e269e9a917ccd49da94f05b47df4f
tomkobyluch <tomkobyluch@gmail.com>
2014-08-27 16:45:10 +0000

    Finished the hw

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args){
+        
+//  print displays welcome screen
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println(" ^  ^  ^  ^  ^  ^  ^");
+    System.out.println("/"+" \\"+"/"+" \\"+"/"+" \\"+"/"+" \\"+"/"+" \\"+"/"+" \\"+"/"+" \\");
+    System.out.println("| -T--P--K--2--1--8- |");
+    System.out.println("\\"+" /"+"\\"+" /"+"\\"+" /"+"\\"+" /"+"\\"+" /"+"\\"+" /"+"\\"+" /");
+    System.out.println(" v  v  v  v  v  v  v ");
+    System.out.println("This summer I went to several" + "\n" +" beaches and played alot of airsoft.");
+    
+    }
+}
