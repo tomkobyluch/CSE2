53d54b97dbfe7829de4f8efdd30286d3ebd1748b
tomkobyluch <tomkobyluch@gmail.com>
2014-09-03 00:59:35 +0000

    Cleaned up hw01

--- hw01/WelcomeClass.java
+++ hw01/WelcomeClass.java
@@ -4,9 +4,9 @@ public class WelcomeClass{
     public static void main(String[] args){
         
 //  print displays welcome screen
-    System.out.println("-----------");
-    System.out.println("| WELCOME |");
-    System.out.println("-----------");
+    System.out.println("     -----------");
+    System.out.println("     | WELCOME |");
+    System.out.println("     -----------");
     System.out.println(" ^  ^  ^  ^  ^  ^  ^");
     System.out.println("/"+" \\"+"/"+" \\"+"/"+" \\"+"/"+" \\"+"/"+" \\"+"/"+" \\"+"/"+" \\");
     System.out.println("| -T--P--K--2--1--8- |");
