ec9cab1417126d05d9524dba7109d5d73c6afe3d
tomkobyluch <tomkobyluch@gmail.com>
2014-09-10 00:01:08 +0000

    added total tax output

--- hw02/Arithmetic.java
+++ hw02/Arithmetic.java
@@ -17,11 +17,13 @@ public class Arithmetic{    //make new class
         double envelopetax = envelopetotal * 0.06;  //variable for envelope tax
         double totalcost = ((nsocks*sockcost)+(nglasses*glassescost)+(nenvelopes*envelopecost));    //variable for total cost
         double totalcostwithtax = totalcost + (totalcost*0.06); //variable for total cost with tax
+        double totaltax = totalcost * 0.06;
         
         System.out.printf("Total cost of socks is $%1.2f with sales tax of $%1.2f \n", socktotal, socktax); //outputs the string and uses the two variables listed at end in place of the %1.2f, which cuts decimals to 2
         System.out.printf("Total cost of glasses is $%1.2f with sales tax of $%1.2f \n", glassestotal, glassestax); //outputs the string and uses the two variables listed at end in place of the %1.2f, which cuts decimals to 2
         System.out.printf("Total cost of envelopes is $%1.2f with sales tax of $%1.2f \n", envelopetotal, envelopetax); //outputs the string and uses the two variables listed at end in place of the %1.2f, which cuts decimals to 2
         System.out.printf("Total cost without tax is $%1.2f \n", totalcost); //one way to cut demimal place,this line outputs the computation to get the total cost of the purchase after tax
+        System.out.printf("Total sales tax is $%1.2f \n", totaltax); //outputs the string and cuts decimals, displays total tax
         System.out.printf("Total cost with tax is $%1.2f \n", totalcostwithtax); //one way to cut demimal places, cost with tax
     }
 }
