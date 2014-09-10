5068076244778b9d9df93a05fe84ffdb794b5a58
tomkobyluch <tomkobyluch@gmail.com>
2014-09-10 16:57:12 +0000

    finshed second prob of hw03

--- hw03/Root.java
+++ hw03/Root.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 class Root{
-    public static main(String[] args){
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter a double:");
+        double num = scan.nextDouble();
+        double numcuberoot = Math.pow(num,(0.3333));
+        System.out.println("This is a crude cubed root of that number: " + numcuberoot);
+        System.out.println("This is that crude number cubed: "+ Math.pow(numcuberoot,3));
         
     }
 }
