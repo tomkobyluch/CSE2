8380f50198194004038cd2413f59c7b29234b475
tomkobyluch <tomkobyluch@gmail.com>
2014-09-17 16:53:08 +0000

    formatted better

--- lab04/BigMacAgain.java
+++ lab04/BigMacAgain.java
@@ -10,25 +10,47 @@ class BigMacAgain{
         
         System.out.println("How many Big Macs?:");
         
-        if(scan.hasNextInt()){numbigmacs = scan.nextInt(); System.out.print("\nYou ordered "+ numbigmacs); bigmacprice = numbigmacs * 2.22; System.out.printf(". Total cost of burgers is $%1.2f \n", bigmacprice);}
+        //for burgers
+        if(scan.hasNextInt()){
+            numbigmacs = scan.nextInt(); 
+            System.out.print("You ordered "+ numbigmacs); 
+            bigmacprice = numbigmacs * 2.22; 
+            System.out.printf(". Total cost of burgers is $%1.2f \n", bigmacprice);
+            
+        }
         else{
             System.out.println("Error: You didn't enter an integer.");
             return;
         }
         
         System.out.println("Would you like fries?");
-        
         double total = bigmacprice + 2.15;
         
+        //for fries
         char friesanswer = scan.next().charAt(0);
         switch(friesanswer){
-            case 'Y': System.out.println("Fries cost $2.15");total = bigmacprice + 2.15;System.out.printf("Total cost $%1.2f \n", total);break;
-            case 'y': System.out.println("Fries cost $2.15");total = bigmacprice + 2.15;System.out.printf("Total cost $%1.2f \n", total);break;
-            case 'N': total = bigmacprice;System.out.printf("Total cost $%1.2f \n", total);break;
-            case 'n': total = bigmacprice;System.out.printf("Total cost $%1.2f \n", total);break;
-            default:System.out.println("Error: Enter Y,y,N,or n.");return;
+            
+            case 'Y': System.out.println("Fries cost $2.15");
+            total = bigmacprice + 2.15;
+            System.out.printf("Total cost $%1.2f \n", total);
+            break;
+            
+            case 'y': System.out.println("Fries cost $2.15");
+            total = bigmacprice + 2.15;
+            System.out.printf("Total cost $%1.2f \n", total);
+            break;
+            
+            case 'N': total = bigmacprice;
+            System.out.printf("Total cost $%1.2f \n", total);
+            break;
+            
+            case 'n': total = bigmacprice;
+            System.out.printf("Total cost $%1.2f \n", total);
+            break;
+            
+            default:System.out.println("Error: Enter Y,y,N,or n.");
+            return;
         }
-
         
     }
 }
