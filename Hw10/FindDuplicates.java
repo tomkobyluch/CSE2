b5f2de0895c9db5501622450874c94943e89027a
tomkobyluch <tomkobyluch@gmail.com>
2014-11-19 03:56:41 +0000

    not done commenting

--- Hw10/FindDuplicates.java
+++ Hw10/FindDuplicates.java
@@ -1,24 +1,30 @@
-import java.util.Scanner;
-public class FindDuplicates{
-  public static void main(String [] arg){
-    Scanner scan=new Scanner(System.in);
-    int num[]=new int[10];
-    String answer="";
-    do{
-      System.out.print("Enter 10 ints- ");
-      for(int j=0;j<10;j++){
+//Tom Kobyluch
+//11-18-14
+//HW10
+//This program take in ten integers and check them against each other to see if there are duplicates. If there is exactly one duplicate that must be confirmed.
+//
+
+import java.util.Scanner; //import scanner class
+public class FindDuplicates{  //class declare
+  public static void main(String [] arg){ //main statement declared
+    Scanner scan=new Scanner(System.in);  //create new scanner
+    int num[]=new int[10];  //create array for the input
+    String answer=""; //
+    do{ //main loop for asking user to go again
+      System.out.print("Enter 10 ints- ");  //prompt user
+      for(int j=0;j<10;j++){  //take in all the integers and store them in array
         num[j]=scan.nextInt();
       }
-      String out="The array ";
+      String out="The array ";  //output numbers
       out+=listArray(num); //return a string of the form "{2, 3, -9}"   
-      if(hasDups(num)){
+      if(hasDups(num)){ //if it has dups display this
         out+="has ";
       }
       else{
-        out+="does not have ";
+        out+="does not have ";  //else display this
       }
       out+="duplicates.";
-      System.out.println(out);
+      System.out.println(out);  //output the completed string
       out="The array ";
       out+=listArray(num);    
       if(exactlyOneDup(num)){
@@ -29,14 +35,14 @@ public class FindDuplicates{
       }
       out+="exactly one duplicate.";
       System.out.println(out);
-System.out.print("Go again? Enter 'y' or 'Y', anything else to quit- ");
-      answer=scan.next();
-    }while(answer.equals("Y") || answer.equals("y"));
+System.out.print("Go again? Enter 'y' or 'Y', anything else to quit- ");  //ask user if they want to go agian
+      answer=scan.next();   //take in the users answer
+    }while(answer.equals("Y") || answer.equals("y")); //restart loop only if answer is Y or y
   }
 
-  public static String listArray(int num[]){
+  public static String listArray(int num[]){  //method for display arrays
     String out="{";
-    for(int j=0;j<num.length;j++){
+    for(int j=0;j<num.length;j++){  //print each elemnt of array with comma inbetween, {} at end
       if(j>0){
         out+=", ";
       }
@@ -46,32 +52,32 @@ System.out.print("Go again? Enter 'y' or 'Y', anything else to quit- ");
     return out;
   }
   
-  public static boolean hasDups(int[] num){
-      boolean dupes = false;
-      for(int i = 0;i<num.length;i++){
-          for(int x = 0;x<num.length;x++){
+  public static boolean hasDups(int[] num){   //checks if there is dupe in array
+      boolean dupes = false;  
+      for(int i = 0;i<num.length;i++){  //check every elemnt of array
+          for(int x = 0;x<num.length;x++){  //against every other element of the array
               if(i != x && num[i] == num[x]){   //makes sure it doesnt say itself is dupe
-                  dupes = true;
+                  dupes = true; //only if dupe is found willl dupes boolena be switched to true
               }
           }
       }
-      return dupes;
+      return dupes; //return bollean
   }
   
-  public static boolean exactlyOneDup(int[] num){
+  public static boolean exactlyOneDup(int[] num){ //check if there is only one dupe
       boolean dupes = false;
       int counter = 0;
-      for(int i = 0;i<num.length;i++){
-          for(int x = i;x<num.length;x++){
+      for(int i = 0;i<num.length;i++){//check every element of array
+          for(int x = i;x<num.length;x++){  //against every other element
               if(i != x && num[i] == num[x]){   //makes sure it doesnt say itself is dupe
-                  counter++;
+                  counter++;  //add to counter for every dupe
               }
           }
       }
-      if(counter == 1){
-          dupes = true;
+      if(counter == 1){ //only is counter is 1 will it return true, 1 duplicate.
+          dupes = true; //set true if one dupe
       }
-      return dupes;
+      return dupes; //return boolean
   }
   
 }
--- Hw10/PokerOdds.java
+++ Hw10/PokerOdds.java
@@ -1,13 +1,20 @@
-import java.util.Random;
-import java.util.Scanner;
-public class PokerOdds{
-  public static void main(String [] arg){
-    showHands();
-    simulateOdds();
+//Tom Kobyluch
+//11-18-14
+//HW10
+//This program first gives the user a random hand of 5 cards from a standard deck of cards and keeps gives new hands until the user says no. After this the odds of getting 1 pair of 
+//each card in a hand are computed over 10000 hands
+//
+
+import java.util.Random;        //imopoprt random class
+import java.util.Scanner;   //import scanner class
+public class PokerOdds{ //declare calss
+  public static void main(String [] arg){   //declare main method
+    showHands();    //run the random rands given section
+    simulateOdds(); //run the 10000 odds section
   }
   
-  public static void simulateOdds(){
-int notdupes = 0;
+  public static void simulateOdds(){    //simulates odds of getting 1 pair of certain card over 10000 hands
+int notdupes = 0;           //all theses counters are used to keep track of which card had a pair
       int acecounter = 0;
       int kingcounter = 0;
       int queencounter = 0;
@@ -22,7 +29,7 @@ int notdupes = 0;
       int nine = 0;
       int ten = 0;
       
-      for(int y = 0;y<10000;y++){    //j is the current card we are checking for pairs
+      for(int y = 0;y<10000;y++){    //main loop that runs everything 10000 times
         int[] deck = new int[52]; //creat new deck
         for(int i = 0;i<deck.length;i++){     //fills my deck
           deck[i] = i;
@@ -31,31 +38,31 @@ int notdupes = 0;
         int[] hand = new int[5];      //code section to get hand
         Random rand = new Random();
         int position = 0;
-        for(int x = 0; x<hand.length;x++){
-            position = rand.nextInt(52); 
+        for(int x = 0; x<hand.length;x++){  //will give each elemnt of 5 length array a random number
+            position = rand.nextInt(52);    //position refers to position in the main deck
             while(deck[position] == -1){    //this loop checks if the number in the deck is taken, and gets new random if it is
-                position = rand.nextInt(52); 
+                position = rand.nextInt(52); //new random if pos taken
             }
-            hand[x] = deck[position];
-            deck[position] = -1;
+            hand[x] = deck[position];   //sets array value
+            deck[position] = -1;    //sets deck element to -1 to give the loop an indication of whats already taken
       }
       
       String[] deckstring = new String[52]; //converted deck to have all the proper numbers and letters
       for(int i = 0;i<deckstring.length;i++){     //fills my deck
-          deckstring[i] = Integer.toString(i-2);
+          deckstring[i] = Integer.toString(i-2);    //all the numbers under 13 just need to be decreased by 2
           if(i>13){
-              deckstring[i] = Integer.toString((i%13)-2);
+              deckstring[i] = Integer.toString((i%13)-2);   //all the ones over that are decreades by 2 after mod 13
           }
-          if(i%13==0){
+          if(i%13==0){              //0 is for aces
               deckstring[i]="A";
           }
-          if(i%13==1){
+          if(i%13==1){              //remainder 1 is for kings
               deckstring[i]="K";
           }
-          if(i%13==2){
+          if(i%13==2){              //remainder 2 is for queens
               deckstring[i]="Q";
           }
-          if(i%13==3){
+          if(i%13==3){              //remainder 3 is for jacks
               deckstring[i]="J";
           }
           
@@ -68,15 +75,15 @@ int notdupes = 0;
       //String stringj = deckstring[j];   //convert my card in number form to string form to check against my string form deck
       
       String[] stringhand = new String[5];  //converting array hand to 2-10 and a,k,q,j to easily check for pairs
-      for(int i = 0;i<5;i++){
-          stringhand[i] = deckstring[hand[i]];
+      for(int i = 0;i<5;i++){   //for every elemnt in the hand
+          stringhand[i] = deckstring[hand[i]];  //convert its orig position to new one in the string deck
       }
       
-      outerloop:
-      for(int i = 0;i<hand.length;i++){
-          for(int x = i+1;x<hand.length;x++){
-              if(i != x && stringhand[i].equals(stringhand[x])){   //makes sure it doesnt say itself is dupe
-                  if(stringhand[i].equals("A")){        
+      outerloop:    //jump for hopping out of 2 loops
+      for(int i = 0;i<hand.length;i++){ //loops checks every elemnt in hand
+          for(int x = i+1;x<hand.length;x++){   //against all the other elemtns to check for dupe
+              if(i != x && stringhand[i].equals(stringhand[x])){   //makes sure it doesnt say itself is dupe and check if equal
+                  if(stringhand[i].equals("A")){            //all these if statemtns check which card has the duplicate and adds to the cards respective counter     
                       acecounter++;
                       break outerloop;
                   }
@@ -133,7 +140,7 @@ int notdupes = 0;
           }
       }
       }
-      System.out.println("Aces: "+ acecounter);
+      System.out.println("Aces: "+ acecounter);     //these output the numbers of duplicates in the 10000 hands
       System.out.println("Kings: "+kingcounter);
       System.out.println("Queens: "+queencounter);
       System.out.println("Jacks: "+jackcounter);
@@ -149,11 +156,11 @@ int notdupes = 0;
       System.out.println("Not Dupes: "+ Integer.toString(10000-(acecounter+kingcounter+queencounter+jackcounter+twocounter+three+four+five+six+seven+eight+nine+ten)));
     }
   
-  public static void showHands(){
+  public static void showHands(){   //method to show users random hands
       boolean control = true;
       Scanner scan = new Scanner(System.in);
-      do{
-      int[] deck = new int[52]; 
+      do{   //loops repeats until user says no
+      int[] deck = new int[52];     //make a new deck
       for(int i = 0;i<deck.length;i++){     //fills my deck
           deck[i] = i;
           
@@ -161,33 +168,33 @@ int notdupes = 0;
       
       int[] hand = new int[5];//code section to get hand
       Random rand = new Random();
-      int position = 0;
-      for(int x = 0; x<hand.length;x++){
-        position = rand.nextInt(52); 
+      int position = 0; 
+      for(int x = 0; x<hand.length;x++){    //sets 5 elements of the array
+        position = rand.nextInt(52); //rand vbalue from deck array
         while(deck[position] == -1){    //this loop checks if the number in the deck is taken, and gets new random if it is
             position = rand.nextInt(52); 
         }
-        hand[x] = deck[position];
-        deck[position] = -1;
+        hand[x] = deck[position];   //set array value
+        deck[position] = -1; pos refers to pos in deck
       }
       
       String[] deckstring = new String[52]; //converted deck to have all the proper numbers and letters
       for(int i = 0;i<deckstring.length;i++){     //fills my deck
-          deckstring[i] = Integer.toString(i-2);
+          deckstring[i] = Integer.toString(i-2);      //all the numbers under 13 just need to be decreased by 2
           if(i>13){
-              deckstring[i] = Integer.toString((i%13)-2);
+              deckstring[i] = Integer.toString((i%13)-2);  //all the ones over that are decreades by 2 after mod 13 to convert from 0-52 to 2-10
           }
           if(i%13==0){
-              deckstring[i]="A";
+              deckstring[i]="A";//remainder 0 is for aces
           }
           if(i%13==1){
-              deckstring[i]="K";
+              deckstring[i]="K";//remainder 1 is for kings
           }
           if(i%13==2){
-              deckstring[i]="Q";
+              deckstring[i]="Q";//remainder 2 is for queens
           }
           if(i%13==3){
-              deckstring[i]="J";
+              deckstring[i]="J";//remainder 3 is for jacks
           }
           
       }
@@ -219,9 +226,9 @@ int notdupes = 0;
               System.out.print(deckstring[hand[i]] + "  ");
           }
       }
-      System.out.println("\nAnother hand? (Y or y for yes | Anything else for no)");
+      System.out.println("\nAnother hand? (Y or y for yes | Anything else for no)");    //prompt user
       char answer = scan.next().charAt(0);
-      if(answer == 'Y' || answer == 'y'){
+      if(answer == 'Y' || answer == 'y'){   //if user enters y or Y continue, else quit
       }else{
           control = false;
       }
